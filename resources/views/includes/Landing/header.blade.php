592658f87eb3a63dc6bc2fcc9c3e8c5580898579
endijulian <endijulian080798@gmail.com>
2022-06-21 20:55:44 +0700

    slicing dashboard

--- resources/views/includes/Landing/header.blade.php
+++ resources/views/includes/Landing/header.blade.php
@@ -56,7 +56,13 @@
                 <div @click.away="open = false" class="hidden lg:block relative" x-data="{ open: false }">
                     <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
                         Hallo, {{ Auth::user()->name }}
-                        <img class="inline ml-3 h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt="">
+
+                        @if (Auth::user()->detail_user->photo != null)
+                            <img class="inline ml-3 h-12 w-12 rounded-full" src="{{ Storage::url(Auth::user()->detail_user->photo) }}" alt="">
+                        @else
+                            <img class="inline ml-3 h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt="">
+                        @endif
+
                         <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </button>
 
